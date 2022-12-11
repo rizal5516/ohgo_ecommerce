2197161944a9bda2085077b9aab175867651c0f0
rizal5516 <rizalqowi17@gmail.com>
2022-12-11 20:57:32 +0700

    First Commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderEcom />
+    <HeroEcom />
+    <WomenBannerEcom />
+    <InstaEcom />
+    <PartnerEcom />
+    <FooterEcom />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderEcom from '@/components/HeaderEcom.vue';
+import HeroEcom from '@/components/HeroEcom.vue';
+import WomenBannerEcom from '@/components/WomenBannerEcom.vue';
+import InstaEcom from '@/components/InstaEcom.vue';
+import PartnerEcom from '@/components/PartnerEcom.vue';
+import FooterEcom from '@/components/FooterEcom.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderEcom,
+    HeroEcom,
+    WomenBannerEcom,
+    InstaEcom,
+    PartnerEcom,
+    FooterEcom
+  }
+}
+</script>
